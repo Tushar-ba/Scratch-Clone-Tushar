173eae475e0911c3f4e8eea1e4b97908af7b5f05
Tushar-ba <tusharba321@gmail.com>
2024-08-02 07:18:38 +0530

    created dragabble logic

--- src/components/DraggableBlock.jsx
+++ src/components/DraggableBlock.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
 
-const DraggableBlock = () => {
+const DraggableBlock = ({ label, index, moveBlock, deleteBlock }) => {
+  const [, ref] = useDrag({
+    type: "block",
+    item: { index },
+  });
+  const [, drop] = useDrop({
+    accept: "block",
+    hover: (item) => {
+      if (item.inedex != index) {
+        moveBlock(item.index, index);
+        item.index = index;
+      }
+    },
+  });
   return (
-    <div>DraggableBlock</div>
-  )
-}
+    <div
+      ref={(node) => ref(drop(node))}
+      className="p-2 border bg-gray-100 flex justify-between items-center"
+    >
+      <span>{label}</span>
+      <button onClick={() => deleteBlock(index)}>Delete</button>
+    </div>
+  );
+};
 
-export default DraggableBlock
+export default DraggableBlock;
