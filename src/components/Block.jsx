8bf05cb1448f3343d78d4288bc0f7661f01f54da
Tushar-ba <tusharba321@gmail.com>
2024-08-02 07:01:15 +0530

    Created block idetification logic in block.jsx and to monitor if the block is being dragged or not

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useDrag } from "react-dnd";
 
-const Block = () => {
+const Block = ({ label, type, onClick }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type,
+    item: { label },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
   return (
-    <div>Block</div>
-  )
-}
+    <div
+      ref={drag}
+      className={`p-2 border ${isDragging ? "bg-gray-300" : "bg-gray-100"}`}
+      onClick={() => onClick(label)}
+    >
+      {label}
+    </div>
+  );
+};
 
-export default Block
+export default Block;
